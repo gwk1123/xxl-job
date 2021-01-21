241da6096afeca34fbd1239069dab0a64b6c4097
gwk1123 <g15625281551@163.com>
2021-01-21 10:24:17 +0800

    启动配置9

--- xxl-job-admin/docker/startup.sh
+++ xxl-job-admin/docker/startup.sh
@@ -1,4 +1,4 @@
 APPNAME=xxljobadmin
-PORT=8092
+PORT=8085
 docker build -t $APPNAME .
-docker run -d --name $APPNAME -p $PORT:$PORT -v /logs/applogs:/data/applogs  $APPNAME
+docker run -itd --name $APPNAME -p $PORT:$PORT -v /logs/applogs:/data/applogs  $APPNAME
